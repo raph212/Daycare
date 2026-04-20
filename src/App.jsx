26af0fd741dc72a62beedb8aa4d211cf40a6bd48
raph212 <roshamonb@gmail.com>
2026-04-20 17:37:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Layout from "./Sidebar";
+
+export default function App() {
+  return (
+    <Layout>
+      <p className="text-gray-400">Dashboard content goes here</p>
+    </Layout>
+  );
+}
